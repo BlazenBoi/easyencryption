5e5a25d00c03e30bbbdcafa0ce47500792eb7a78
BlazenBoi <65360471+BlazenBoi@users.noreply.github.com>
2023-06-19 10:47:53 -0600

    Github Update for ReadMe update with PyPi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_desc = open("README.md", "r")
 long_description = long_desc.read()
 ##with open('src/easyencryption/__init__.py', 'r') as f:
     ##version = [line.split('=')[1].strip(" '\"") for line in f.read().splitlines() if line.startswith('__version__')][0]
-version = "0.1.5"
+version = "0.1.5.1"
 
 setup(
     name='easyencryption',
