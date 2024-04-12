1ce38e5b8fe302f5511d416fec63d8e23b720e98
BlazenBoi <65360471+BlazenBoi@users.noreply.github.com>
2024-04-12 10:41:08 -0500

    Removed Redundant Files

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import pathlib
 here = pathlib.Path(__file__).parent.resolve()
 long_desc = open("README.md", "r")
 long_description = long_desc.read()
-version = "0.1.8.8"
+version = "0.1.8.9"
 
 setup(
     name='easyencryption',
